b6cac2957af9123d22838bc34e6d8a4cdd29fdbc
Koki Goshima <gosh.namnam0705@gmail.com>
2019-05-31 01:27:18 +0800

    ScriptController作成

---
+++ app/Http/Controllers/ScriptController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ScriptController extends Controller
+{
+    public function index(){
+
+    }
+
+    public function store(){
+    	
+    }
+
+    public function edit(){
+    	
+    }
+
+    public function update(){
+    	
+    }
+
+    public function showSample(){
+    	
+    }
+
+    public function destroy(){
+    	
+    }
+}
