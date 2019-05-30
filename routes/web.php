17b7a8575ab52a6db6eb545588b6416c0b8b1c83
Koki Goshima <gosh.namnam0705@gmail.com>
2019-05-31 01:16:53 +0800

    web.php作成

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,25 @@
 //     return view('welcome');
 // });
 
-Auth::routes();
 
 // Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/', 'HowToController@index');
+
+Auth::routes();
+
+Route::group(["middleware" => "auth"], function(){
+
+	Route::get("scripts/", "ScriptController@index"); //ファイル一覧画面
+
+	Route::post("scripts/create", "ScriptController@store"); //新規作成処理
+
+	Route::get("scripts/{param}/edit", "ScriptController@edit"); //編集画面
+
+	Route::patch("scripts/{param}/update", "ScriptController@update"); //更新処理
+
+	Route::get("scripts/sample", "ScriptController@showSample"); //サンプル画面
+
+	Route::delete("diary/{param}/delete", 'DiaryController@destroy');
+
+});
