257801f5bad8967ada6df72a30625a71956cf3d0
Koki Goshima <gosh.namnam0705@gmail.com>
2019-05-31 01:53:53 +0800

    howto画面遷移処理

--- app/Http/Controllers/ScriptController.php
+++ app/Http/Controllers/ScriptController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class ScriptController extends Controller
 {
     public function index(){
-
+    	
     }
 
     public function store(){
--- resources/views/scripts/index.blade.php
+++ resources/views/scripts/index.blade.php
@@ -1,10 +1,3 @@
-<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<link rel="stylesheet" href="yourFiles.css"> <!-- 追加 -->
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous"> <!-- 追加 -->
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
-<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-<script type="text/javascript" src="yourFiles.js"></script> <!-- 追加 -->
-<!------ Include the above in your HEAD tag ---------->
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -13,6 +6,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"> -->
+  <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+  <link rel="stylesheet" href="yourFiles.css"> <!-- 追加 -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous"> <!-- 追加 -->
 </head>
 <body>
   <section id="wrapper">
@@ -88,5 +84,8 @@
   </section>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<script type="text/javascript" src="yourFiles.js"></script> <!-- 追加 -->
 </body>
 </html>
