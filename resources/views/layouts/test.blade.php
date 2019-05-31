86919304dec49c487b155f7b3fab29cbf367f060
タミヤナオキ <n03.2ri5to3@gmail.com>
2019-05-31 20:44:38 +0800

    タミヤ　ヘッダー・フッター追加

---
+++ resources/views/layouts/test.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>作成画面 | Script maker</title>
+    <!-- css/reset.cscとstyle.cssは触らないように-->
+    <link rel="stylesheet" href="{{asset('css/test.css')}}">
+    <link rel="stylesheet" href="{{asset('css/reset.css')}}">
+    <link href="https://fonts.googleapis.com/css?family=Anton" rel="stylesheet">
+</head>
+
+  <!--ヘッダー-->
+  <header>
+    <nav>
+      <div class="logo">
+      <a href="#">Script maker</a>
+      </div>
+      <ul class="clearfix">
+        <li><a href="#"><img src="{{asset('img/account.png')}}" alt="account"></a></li>
+        <li><a href="#" >マイスクリプト</a></li>
+        <li><a href="#" >完成例一覧</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <!--コンテンツ-->
+  <body>
+    <div id="contents">
+      <!-- コンテンツをここに書いてください -->
+    </div>  
+  </body>
+
+  <!--フッター-->
+  <footer>
+    <p class="copyright"><small>Copyright (C) 2019 Nohohon All rights reserved</small></p>
+  </footer>
+
+</body>
+</html>
