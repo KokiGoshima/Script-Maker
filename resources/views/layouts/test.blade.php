0497aebf35acbd7f0291a80bec5d949717eddb2b
Koki Goshima <gosh.namnam0705@gmail.com>
2019-06-01 14:06:20 +0800

    各ページcss機能追加

--- resources/views/layouts/test.blade.php
+++ resources/views/layouts/test.blade.php
@@ -15,6 +15,8 @@
     <link rel="stylesheet" href="{{asset('css/test.css')}}">
     <link rel="stylesheet" href="{{asset('css/reset.css')}}">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    @yield("indexCss")
+    @yield("showSampleCss")
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
