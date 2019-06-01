5f3f32b9aba9ca0ea2108260984abe51f4f480c2
KakeyaTatsushi <lt.tatsu32@gmail.com>
2019-06-01 14:51:23 +0800

    index.cssにjs追加

--- resources/views/scripts/index.blade.php
+++ resources/views/scripts/index.blade.php
@@ -73,7 +73,7 @@
   </section>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <script type="text/javascript" src="index.js"></script> <!-- 追加 -->
+  <script type="text/javascript" src="js/index.js"></script> <!-- 追加 -->
 @endsection
 {{-- </body>
 </html> --}}
