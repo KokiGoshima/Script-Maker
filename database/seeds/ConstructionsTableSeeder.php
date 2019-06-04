736f7a90165ba9c40937b9aa65fdeb8075e3d31b
Koki Goshima <gosh.namnam0705@gmail.com>
2019-06-04 10:16:56 +0800

    アコーディオンseederの作成終わり

---
+++ database/seeds/ConstructionsTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ConstructionsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $constructions = [
+    		[
+    			"flow" => "導入"
+    		],
+    		[
+    			"flow" => "本論"
+    		],
+    		[
+    			"flow" => "結論"
+    		]
+    	];
+
+    	foreach ($constructions as $construction) {
+    		DB::table("constructions")->insert([
+    			"flow" => $construction["flow"],
+    			"created_at" => Carbon::now(),
+    			"updated_at" => Carbon::now()
+    		]);
+    	}
+    }
+}
---
+++ database/seeds/SituationsTableSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SituationsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $situations = [
+    		[
+    			"pattern" => "挨拶",
+    			"flow_id" => 1
+    		],
+    		[
+    			"pattern" => "理由2",
+    			"flow_id" => 2
+    		],
+    		[
+    			"pattern" => "締めくくり",
+    			"flow_id" => 3
+    		]
+    	];
+
+    	foreach ($situations as $situation) {
+    		DB::table("situations")->insert([
+    			"pattern" => $situation["pattern"],
+          "flow_id" => $situation["flow_id"],
+    			"created_at" => Carbon::now(),
+    			"updated_at" => Carbon::now()
+    		]);
+    	}
+    }
+}
