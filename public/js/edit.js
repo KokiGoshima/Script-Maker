700bda75f48f33ac0f57f1719c047d72d14e9245
Koki Goshima <gosh.namnam0705@gmail.com>
2019-06-09 17:32:07 +0800

    途中

--- public/js/edit.js
+++ public/js/edit.js
@@ -6,7 +6,16 @@ $(function(){
   if (confirm('現在のスクリプト内容を保存してもよろしいですか？')) {
   	document.getElementById("form_" + this.dataset.id).submit();
    }
-  })
+  });
+
+  $("body").on("click", function(){
+  	$("#save").hide();
+  });
+
+  // $("body").click(function(){
+  // 	$("#save").fadeOut();
+  // });
 
 // onclick="return confirm('現在のスクリプト内容を保存してもよろしいですか？')"
 })
+
