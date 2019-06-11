7a3b06fac457de26c7884ccfa0d5b8438643a77b
Koki Goshima <gosh.namnam0705@gmail.com>
2019-06-11 10:59:32 +0800

    途中

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if (\App::environment('heroku')) {
+        \URL::forceScheme('https');
+    }
+        
         Schema::defaultStringLength(191);
     }
 }
