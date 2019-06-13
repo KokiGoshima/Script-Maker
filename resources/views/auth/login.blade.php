d9c946d518692674873ad557c5fc63d370ea7200
KakeyaTatsushi <lt.tatsu32@gmail.com>
2019-06-13 17:03:23 +0800

    ログイン、新規登録画面　フェードアウト

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,7 +17,7 @@
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
+                    <form method="POST" action="{{ route('login') }}" name="login">
                         @csrf
 
                         <div class="form-group row">
