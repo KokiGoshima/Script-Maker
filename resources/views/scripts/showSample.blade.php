6c33e873b8ef24f6bb47b20d719b45ba7a8a4f92
KakeyaTatsushi <lt.tatsu32@gmail.com>
2019-06-19 09:42:45 +0800

    showsample.php 編集

--- resources/views/scripts/showSample.blade.php
+++ resources/views/scripts/showSample.blade.php
@@ -21,11 +21,28 @@
         <div class="copy">
           <button class="btn btn-outline-primary"onclick="copy1()"><i class="far fa-copy"></i>全英文コピー</button>
         </div>
-        <div class="title">タイトル</div>
-        <span id="english-script1">hello, world</span>
-        <br>
+        <div class="title">会社説明会</div>
+        <span id="english-script1">Good afternoon ladies and gentlemen. Welcome to company  information session. On behalf of XXX, I would like to welcome you here today.  My talk will be in three parts.  First is company's outline. Second is corporate philosophy. Third is Recruitment information. Now let's begin.
+    First, I will talk about company's outline. Our company  is engineering and English teaching school. In engineering class, we teach programming. Students finally can  learn how to make an application in  our  class. In English class,  students learn all skills English. All teachers are talented at teaching English. By learning these two things, students can have a skill to work in other countries.
+    Second, corporate philosophy. We really want as many people as to make triggers to work in foreign countries. This is because we think Japanese have not contribute to worlds even though they have much potential to do it. We want to help them to take one step.
+    Third, Recruitment information. We are looking for such human resources like them
+1. Who is interested in teaching
+2. Who want to experience challenging work in foreign countries
+3. Who is always eager to meet new people.
+If even one applies to you, you have chance to join us. If you get interested in our company, please mail here.
+    We are coming to the end of today’s presentation.  If you have any questions, I’d be happy to answer them. Thank you.</span>
+        <br><br>
+        <div class="title">日本語訳</div>
         <div class="japanese-script">
-          あああああ
+          みなさんこんにちは。本日は私たちの会社説明会へようこそ。弊社XXXを代表して、本日皆様を歓迎したいと思います。私の話は主に三つで構成されます。一つ目が会社概要。二つ目が企業理念。三つ目が採用情報です。では始めましょう。
+          まず、会社概要についてお話したいと思います。私たちの会社はエンジニアと英語を教えることを軸としています。エンジニアクラスでは、プログラミングを教えています。生徒たちは最終的にどのように一つのアプリケーションを作るのかを学びます。英語クラスでは、英語スキルにおけるすべてのスキルを学びます。先生は全員経験のある先生たちで成り立っています。プログラミングと英語の二つを学ぶことで、生徒は海外でも働けるようなスキルを手にいれることができます。
+          次に、企業理念についてです。私たちは出来るだけ多くの生徒に世界で活躍する場を提供したいと心から願っております。なぜなら、日本人は世界に貢献できるほどの素質が備わっているにも関わらずそれをまだ活かしきれていないと考えるからです。私たちは彼らに一歩踏み出せるように背中を押してあげたいと考えています。
+          三つ目が、採用情報についてです。私たちが必要としている人材は次のような人です
+          1. 教えることに興味がある人
+          2. 海外で大きいチャレンジを成し遂げたい人
+          3. 常に誰か新しい人に会うことを望んでいる人
+          もしこれらどれかに一つでも当てはまっていれば応募する資格があります。少しでも興味があれば、こちらまでメールをお願い致します。
+          これで発表を終わります。もし質問があれば、喜んでお答え致します。ありがとうございました。
         </div>
       </div>
     </slide>
@@ -39,7 +56,8 @@
         <span id="english-script2">Good afternoon ladies and gentlemen. Welcome to XXX New product's presentation. On behalf of Company I would like to welcome you here today. It is a great honor to be given this opportunity to present here about our new product. My presentation will take about 30 minutes. Please interrupt if you have any questions. Now, let's begin.
     Everybody, how much do you know about yourselves?  What relates to yourself, you know better than anyone. That's what you think. Then, I'll give you some questions about you. Since you entered your company, how much money have you earned? How much have you paid in taxes? Suppose you want to know exactly, then, how would you proceed? When you want to know about your taxes, you'd go to the tax office. But when you want to know your lifetime earnings, how would you find out? The Ministry of Labo? If you check your taxes, you'd be worried about your future pension. You have to inquire the Japanese pension system. How about the insurance? How bother it is! No way, we don't need to check because the government is managing that very well, maybe. For most people, it comes to this. So what happened? Your pension have disappeared. It's a bit too late to blame it on the government. What's the cause? As I told you, it's a bother. So we have made this product. It is YYY. The interface is simple. Let's use it a little. How much monthly pension shall I receive at 65? You type it as it comes, 65 years old ...pension, for example. You will find it is such a "reasonable" sum. The system is not just about money. The place where I was born. The fact is you can print out at home your certificate of residency. It saves much a lot of money on bureaucracy working hours. You save the money of transportation to the Ward Office.
     So to recap, the YYY is not just a record of the past, but also a potential to chose our future life. It become a road signpost. Well, that’s it from me. Thank you once again for taking the time to join today’s presentation.. </span>
-        <br>
+        <br><br>
+        <div class="title">日本語訳</div>
         <div class="japanese-script">
           皆さんこんにちは。弊社YYYの新商品発表会へようこそ。会社を代表して本日皆様を歓迎します。今ここで弊社の新商品の発表の機会を与えられたことは最高の喜びです。プレゼンテーションは30分ほどになる予定です。それでは始めましょう。
           毎日、あなたはあなた自身のことをどれくらい知っているでしょうか。あなたに関わることなら誰よりも知っているだろう。それが今考えていることだと思います。では皆様にいくつか質問をしようと思います。会社に入ってからどれくらい稼いできましたか？その給料に対してどれくらいの税金を今まで払ってきましたか？あなたがこれらのことを知りたいとき、どのようなプロセスを行いますか？税金のことが知りたいから、税務署へ行くでしょう。しかし、あなたが将来どれくらいもらえるのかを知りたいとなった時、どのようにしますか？厚生労働省へ出向きますか？税金のことについて調べていたら、将来の年金についても心配になってくるでしょう。そうなると日本の年金システムについてから知る必要が出てきますね。さらに保険についても知るには？ああもう面倒臭い！もういいや、私たちが確認しなくても、政府がきっと管理してくれているのでしょう。大半の人はこのように考えますね。その結果どうなりましたか？あなたの年金は消え始めました。その時に政府に不平を言ってももう遅いです。何故このような事態を招いたのでしょうか？先ほども述べたように、”面倒臭い”からです。そこで私たちはこの新商品を開発しました。その名もXXXです。画面はいたってシンプルです。少し触ってみましょう。65歳の時点でいくらの年金がもらえるのか気になったとします。思ったままに検索してみましょう。『65歳、年金』と。意外と堅実的な額だと気づくでしょう。このシステムはお金に関してだけではありません。あなたの出身地なども調べられます。実は、役所からもらうような書類を自宅でコピーすることもできます。役所まで行く時間とそれにかかる交通費を大きく節約できますね。
@@ -51,11 +69,10 @@
     <slide :index="2">
       <div class="example">
         <div class="copy">
-          <button class="btn btn-outline-primary" onclick="copy3()"><i class="far fa-copy"></i>全英文コピー</button>
+          <button class="btn btn-outline-primary" onclick="copy2()"><i class="far fa-copy"></i>全英文コピー</button>
         </div>
-
         <div class="title">小学校に英語教育は必要ない</div>
-        <span id="english-script3">According to some data, a lot of parents try to make their children learn English as easy as possible. They want their children to become international players. The Japanese government has the same idea. This is a new education system which will start in 2020. Now, in elementary school, English education starts from when students are in fifth grade. From 2020, they will start it when they are in third grade.
+        <span id="english-script2">According to some data, a lot of parents try to make their children learn English as easy as possible. They want their children to become international players. The Japanese government has the same idea. This is a new education system which will start in 2020. Now, in elementary school, English education starts from when students are in fifth grade. From 2020, they will start it when they are in third grade.
 
         In my opinion, English education in elementary school is not necessary. There are some reasons, today, I’ll tell you two reasons.
 
@@ -65,7 +82,8 @@
         Two classes a week are not enough to study English. Students have several different classes every day. Under the only two classes, they easily forget what they learn. Let me give you one example. A girl who has studied English since she was in the first grade of an elementary school. She had 2 English classes a week in an elementary school. What happened to her when she was in junior high school. Actuary, her English grade was better than others. However, the wonderful times didn’t last too long. The gap between her and others was gradually closed. And finally, there were no differences. Studying English at an elementary school is good for nothing. It won’t help with anything.
 
         I know English is on its way to becoming the world’s universal language. Japanese children should learn English. This is one reality. But we have another more important reality that we ought to think about. The language is deeply connected to forming a personal identity. Learning a lot of Japanese vocabulary and thinking a lot of things with the vocabulary is very important. If children ignore that process and learn English, they will be like a robot just speak Japanese and English. So I wwant to say is that do not kill your language.</span>
-        <br>
+        <br><br>
+        <div class="title">日本語訳</div>
         <div class="japanese-script">
           ある統計によると、たくさんの親が早い時期から子供に英語教育をさせようとしています。彼らは子供を世界で活躍できる人材にしたいと考えています。日本政府も同じ考えを持っています。これは2020年から始まる新しい教育システムです。現在、小学校では英語の授業は小学5年生から始まっています。2020年からは小学3年生から始まる予定です。
 
@@ -76,33 +94,11 @@
           ２つ目の理由は、1週間に2つの授業は不十分だからです。生徒は毎日様々な授業があります。結果として学んだことをすぐに忘れてしまいます。１つの例を紹介します。彼女は小学一年生の頃から英語を学んできました。彼女は週2時間の英語の授業がありました。彼女が中学生になった時どうなったでしょうか？実際他の生徒より成績は優秀でした。しかし、そんな素晴らしい時間も長くは続きませんでした。他の生徒との差は次第になくなっていきました。そして、ついにその差はなくなりました。小学校での英語教育は何の意味も為しませんでした。
 
           英語が世界の共通言語になりつつあるのは知っています。なので日本の子供たちは英語を学ぶ必要があります。これは１つの現実です。しかし私たちは考えなければならない、さらに大切なことがあります。言語はアイデンティティの形成に深く関わっています。日本語を多く学びたくさんのことについて考えることはとても大切なことです。もし子供たちがこの過程を無視し英語を学んでしまったら彼らは日本語と英語を喋るようにプログラムされただけのロボットのようになってしまいます。つまり私の言いたいことは、あなたの言語を殺すなということです。
-
-        <div class="title">会社説明会</div>
-        <span id="english-script3">Good afternoon ladies and gentlemen. Welcome to company  information session. On behalf of XXX, I would like to welcome you here today.  My talk will be in three parts.  First is company's outline. Second is corporate philosophy. Third is Recruitment information. Now let's begin.    
-    First, I will talk about company's outline. Our company  is engineering and English teaching school. In engineering class, we teach programming. Students finally can  learn how to make an application in  our  class. In English class,  students learn all skills English. All teachers are talented at teaching English. By learning these two things, students can have a skill to work in other countries.
-    Second, corporate philosophy. We really want as many people as to make triggers to work in foreign countries. This is because we think Japanese have not contribute to worlds even though they have much potential to do it. We want to help them to take one step.
-    Third, Recruitment information. We are looking for such human resources like them
-1. Who is interested in teaching
-2. Who want to experience challenging work in foreign countries
-3. Who is always eager to meet new people.
-If even one applies to you, you have chance to join us. If you get interested in our company, please mail here.
-    We are coming to the end of today’s presentation.  If you have any questions, I’d be happy to answer them. Thank you.</span>
-        <br>
-        <div class="japanese-script">
-          みなさんこんにちは。本日は私たちの会社説明会へようこそ。弊社XXXを代表して、本日皆様を歓迎したいと思います。私の話は主に三つで構成されます。一つ目が会社概要。二つ目が企業理念。三つ目が採用情報です。では始めましょう。
-          まず、会社概要についてお話したいと思います。私たちの会社はエンジニアと英語を教えることを軸としています。エンジニアクラスでは、プログラミングを教えています。生徒たちは最終的にどのように一つのアプリケーションを作るのかを学びます。英語クラスでは、英語スキルにおけるすべてのスキルを学びます。先生は全員経験のある先生たちで成り立っています。プログラミングと英語の二つを学ぶことで、生徒は海外でも働けるようなスキルを手にいれることができます。
-          次に、企業理念についてです。私たちは出来るだけ多くの生徒に世界で活躍する場を提供したいと心から願っております。なぜなら、日本人は世界に貢献できるほどの素質が備わっているにも関わらずそれをまだ活かしきれていないと考えるからです。私たちは彼らに一歩踏み出せるように背中を押してあげたいと考えています。
-          三つ目が、採用情報についてです。私たちが必要としている人材は次のような人です
-          1. 教えることに興味がある人
-          2. 海外で大きいチャレンジを成し遂げたい人
-          3. 常に誰か新しい人に会うことを望んでいる人
-          もしこれらどれかに一つでも当てはまっていれば応募する資格があります。少しでも興味があれば、こちらまでメールをお願い致します。
-          これで発表を終わります。もし質問があれば、喜んでお答え致します。ありがとうございました。
-
         </div>
       </div>
     </slide>
 
+    
     {{-- <slide :index="3">
       <div class="example">
         <div class="copy">
